cb649b26ecf70fe00b3f8e2bad66c9011b8c55d0
Davide Mainas <dmainas@MacBook-Pro-di-Davide.local>
2017-09-20 13:19:28 +0200

    comments index

--- extension/index.js
+++ extension/index.js
@@ -4,11 +4,13 @@ const commands = require('./commands');
 exports.activate = (context) => {
     const registerCommand = vscode.commands.registerCommand
 
+    //Register commands
     const disposableComponent = registerCommand('extension.extractComponentToFile', commands.extractComponentToFile);
     const disposableMethod = registerCommand('extension.extractComponentToFunction', commands.extractComponentToFunction);
     const disposableStyle = registerCommand('extension.extractStyle', commands.extractStyle);
     const disposableEmbed = registerCommand('extension.embedComponent', commands.embedComponent);
 
+    //Push subscriptions to context
     context.subscriptions.push(disposableComponent);
     context.subscriptions.push(disposableMethod);
     context.subscriptions.push(disposableStyle);
